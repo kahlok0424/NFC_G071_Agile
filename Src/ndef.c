cc0a0e788dfeb5019f65534b25eeffa468fa89e6
KahLok <kahlok0424@gmail.com>
2019-07-31 14:05:57 +0800

    small modification

--- Src/ndef.c
+++ Src/ndef.c
@@ -7,14 +7,22 @@
 #include "ndef.h"
 #include "nfc.h"
 
-
-void writeT5TCCFile(ADDRESSING_MODE address_mode){
+/*
+ * @brief Ndef area has to be multiple of 8bytes
+ */
+uint16_t writeT5TCCFile(ADDRESSING_MODE address_mode, uint16_t ndef_area){
 
 	int count;
 	uint8_t buffer[10];
 	buffer[0] = address_mode;
-	buffer[1] = 0x45;
-	buffer[2] = 0x40;
+
+	if( (ndef_area%8) != 0){
+		return NDEF_ERROR;
+	}
+	else{
+		buffer[2] = ndef_area/8;
+	}
+	buffer[1] = 0x40;
 	buffer[3] = 0x01;
 	writeUserMemory(0x00,buffer,4);
 }
